6a0a1ea14031437ca6971a880a708a5e24e2a5c3
Mahbubur Rahman <mahbubur001@gmail.com>
2021-12-06 12:32:23 +0600

    Update config

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 #brew package install
 
-brew install fish
+brew cask install iterm2
+brew install zsh
 brew install git
 brew install neovim
 brew install tmux
@@ -15,13 +16,21 @@ brew install fzf
 stow git
 stow nvim
 stow tmux
-stow fish 
+stow zsh 
 
 
 # add zsh as a login shell
-
+command -v zsh | sudo tee -a /etc/shells
 # use fish as default shell
-chsh -s "$(which fish)"
+sudo chsh -s "$(which zsh)" "$USER"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+upgrade_oh_my_zsh
+
+#Install zsh-z
+git clone https://github.com/agkozak/zsh-z "$ZSH_CUSTOM"/plugins/zsh-z
+
 
 #install  neovim packer plugin
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
