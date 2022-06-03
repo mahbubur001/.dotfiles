5beaf2329d9a6d8209b1ad4c15cef73f9e4f5ec3
Mahbubur Rahman <mahbubur001@gmail.com>
2022-06-03 17:08:37 +0600

    Uidate

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,7 +11,6 @@ set -Ux FZF_CTRL_R_OPTS "--reverse --preview 'echo {}' --preview-window down:3:h
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
 set -Ux FZF_DEFAULT_OPTS "--color=spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
 set -Ux FZF_TMUX_OPTS "-p"
-set -Ux GOPATH (go env GOPATH)
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 
